0b633204fe9dafa7cb5649c07fa9bf054dda5d05
Madelyn Romero <m.romero013@gmail.com>
2020-03-21 18:45:51 -0600

    Add annotations

--- app/controllers/reading_list_items_controller.rb
+++ app/controllers/reading_list_items_controller.rb
@@ -1,10 +1,12 @@
 class ReadingListItemsController < ApplicationController
+  # index action calling on these helper methods and passing an instance variable set to true to the view (the helper methods are creating an algolia search key, and setting to view to either archived or valid)
   def index
     @reading_list_items_index = true
     set_view
     generate_algolia_search_key
   end
 
+  # update action, reaction created (a "like") and saved, if it's the current user's user id
   def update
     @reaction = Reaction.find(params[:id])
     not_authorized if @reaction.user_id != session_current_user_id
@@ -16,6 +18,10 @@ class ReadingListItemsController < ApplicationController
 
   private
 
+  # both called whenever reading list index viewed
+
+  # algolia search key created for current user, given params and the search only key in the application.yml file (that's where we stored it, and this is working, but calling differently, ApplicationConfig instead of ENV)
+  # Where is Algolia being called... still tracking that down
   def generate_algolia_search_key
     params = { filters: "viewable_by:#{session_current_user_id}" }
     @secured_algolia_key = Algolia.generate_secured_api_key(
@@ -23,6 +29,7 @@ class ReadingListItemsController < ApplicationController
     )
   end
 
+  # passing the view a setting of either archived or validate_url
   def set_view
     @view = if params[:view] == "archive"
               "archived"
