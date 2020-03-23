177e3ee6a6bf6b3f3da7d3c4633232f4802020ff
Becky Robran <rerobran@gmail.com>
2020-03-22 19:57:02 -0600

    adds annotations

--- app/javascript/src/components/ItemList/ItemListLoadMoreButton.jsx
+++ app/javascript/src/components/ItemList/ItemListLoadMoreButton.jsx
@@ -1,13 +1,22 @@
 // Load more button for item list
+// Archive / unarchive button for item list
+
+// NOTE: although this element should clearly be a button and not an anchor,
+// I think I've stumbled on a (p)React bug similar to this:
+// <https://github.com/facebook/react/issues/9023>
+// where if I transform it in a button, `e.preventDefault()` in the parent
+// handler or `e.stopPropagation` are just ignored
 import { h } from 'preact';
 import { PropTypes } from 'preact-compat';
-
+// creates functional component ItemListItemArchiveButton with text and onClick props passed down
 export const ItemListLoadMoreButton = ({ show, onClick }) => {
   if (!show) {
     return '';
   }
 
   return (
+    // eslint-disable-next-line jsx-a11y/anchor-is-valid
+    // returns a link that either says archive or unarchive based on the text passed down by ReadingList
     <div className="load-more-wrapper">
       <button onClick={onClick} type="button">
         Load More
@@ -15,7 +24,7 @@ export const ItemListLoadMoreButton = ({ show, onClick }) => {
     </div>
   );
 };
-
+// checks that the data types passed in as props are as expected
 ItemListLoadMoreButton.propTypes = {
   show: PropTypes.bool.isRequired,
   onClick: PropTypes.func.isRequired,
--- app/javascript/src/components/ItemList/ItemListTags.jsx
+++ app/javascript/src/components/ItemList/ItemListTags.jsx
@@ -1,8 +1,9 @@
 // Sidebar tags for item list page
 import { h } from 'preact';
 import { PropTypes } from 'preact-compat';
-
+// creates functional component ItemListTags with availableTags, selectedTags, and onClick for props
 export const ItemListTags = ({ availableTags, selectedTags, onClick }) => {
+  // maps through the availableTags passed down through ReadingList and creates a link for each one, on click, the link fires off the toggleTag function from ReadingList
   const tagsHTML = availableTags.map(tag => (
     <a
       className={`tag ${selectedTags.indexOf(tag) > -1 ? 'selected' : ''}`}
@@ -13,9 +14,10 @@ export const ItemListTags = ({ availableTags, selectedTags, onClick }) => {
       {`#${tag}`}
     </a>
   ));
+  // returns the entire collection of tags to be displayed on the dom
   return <div className="tags">{tagsHTML}</div>;
 };
-
+// checks that the props data types are coming in as expected
 ItemListTags.propTypes = {
   availableTags: PropTypes.arrayOf(PropTypes.string).isRequired,
   selectedTags: PropTypes.arrayOf(PropTypes.string).isRequired,
