6012142c996fe45be08a9261fdc3b225515cffdd
Madelyn Romero <m.romero013@gmail.com>
2020-03-23 12:30:22 -0600

    Add annotations

--- app/javascript/src/components/ItemList/ItemListTags.jsx
+++ app/javascript/src/components/ItemList/ItemListTags.jsx
@@ -2,7 +2,9 @@
 import { h } from 'preact';
 import { PropTypes } from 'preact-compat';
 
+// maps through the availableTags passed down through ReadingList and creates a link for each one, on click, the link fires off the toggleTag function from ReadingList
 export const ItemListTags = ({ availableTags, selectedTags, onClick }) => {
+  // maps through the availableTags passed down through ReadingList and creates a link for each one, on click, the link fires off the toggleTag function from ReadingList
   const tagsHTML = availableTags.map(tag => (
     <a
       className={`tag ${selectedTags.indexOf(tag) > -1 ? 'selected' : ''}`}
@@ -13,9 +15,11 @@ export const ItemListTags = ({ availableTags, selectedTags, onClick }) => {
       {`#${tag}`}
     </a>
   ));
+  // returns the entire collection of tags to be displayed on the dom
   return <div className="tags">{tagsHTML}</div>;
 };
 
+// checks that the props data types are coming in as expected
 ItemListTags.propTypes = {
   availableTags: PropTypes.arrayOf(PropTypes.string).isRequired,
   selectedTags: PropTypes.arrayOf(PropTypes.string).isRequired,
