7cf67fbc5498ab115163aceda735d1ff9548d42c
Madelyn Romero <m.romero013@gmail.com>
2020-03-23 19:52:00 -0600

    Fix test syntax
    Co-Authored-By: rer7891 <rerobran@gmail.com>

--- spec/models/collection_list_spec.rb
+++ spec/models/collection_list_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe CollectionList do
 
   describe "Relationships" do
     it { is_expected.to belong_to(:user).optional }
-    it { has_many :collection_list_articles }
-    it { has_many :articles.through :collection_list_articles }
+    it { is_expected.to have_many :collection_list_articles }
+    it { is_expected.to have_many(:articles).through :collection_list_articles }
   end
 end
