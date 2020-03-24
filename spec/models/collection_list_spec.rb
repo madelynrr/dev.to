b8e9513ab128df69dcd42da35def6bd0a8715638
Madelyn Romero <m.romero013@gmail.com>
2020-03-23 19:16:43 -0600

    Set up tests for belongs to user and many to many with articles
    Co-Authored-By: rer7891 <rerobran@gmail.com>

--- spec/models/collection_list_spec.rb
+++ spec/models/collection_list_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe CollectionList do
   describe "Validations" do
     it { is_expected.to validate_presence_of :name }
   end
+
+  describe "Relationships" do
+    it { is_expected.to belong_to :user }
+    it { has_many :collection_list_articles }
+    it { has_many :articles.through :collection_list_articles }
+  end
 end
