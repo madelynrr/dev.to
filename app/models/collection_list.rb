d511f2db9069a49d69da7c63081b0604584b711c
Madelyn Romero <m.romero013@gmail.com>
2020-03-23 19:49:35 -0600

    Add has many collection list articles and articles through collection list articles
    Co-Authored-By: rer7891 <rerobran@gmail.com>

--- app/models/collection_list.rb
+++ app/models/collection_list.rb
@@ -1,4 +1,7 @@
 class CollectionList < ApplicationRecord
   belongs_to :user, optional: true
   validates :name, presence: true
+
+  has_many :collection_list_articles
+  has_many :articles, through: :collection_list_articles
 end
