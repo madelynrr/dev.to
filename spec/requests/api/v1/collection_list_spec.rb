a73af28a997b28b9e8d7d66a31533d7a1b90b882
Madelyn Romero <m.romero013@gmail.com>
2020-03-24 11:27:18 -0600

    Add user and sign in user before tests
    Co-Authored-By: rer7891 <rerobran@gmail.com>

--- spec/requests/api/v1/collection_list_spec.rb
+++ spec/requests/api/v1/collection_list_spec.rb
@@ -2,6 +2,12 @@ require "rails_helper"
 
 RSpec.describe API do
   describe "Collection Lists" do
+    let(:user) { create(:user) }
+
+    before do
+      sign_in user
+    end
+
     it "can create a collection list" do
       post "/api/v1/collection_lists"
 
