dd700f640b9cc26920d97200cc07e896bf036882
Madelyn Romero <m.romero013@gmail.com>
2020-03-24 11:02:29 -0600

    Add dependent destroy to articles
    Co-Authored-By: rer7891 <rerobran@gmail.com>

--- app/models/collection_list.rb
+++ app/models/collection_list.rb
@@ -4,7 +4,7 @@ class CollectionList < ApplicationRecord
   validates :name, presence: true
 
   has_many :collection_list_articles
-  has_many :articles, through: :collection_list_articles
+  has_many :articles, through: :collection_list_articles, dependent: :destroy
 
   acts_as_taggable_on :tags
 end
