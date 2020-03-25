5845725409b287c2bed9400fdb8eb36fe0a73f23
Becky Robran <rerobran@gmail.com>
2020-03-24 20:26:34 -0600

    adds a helper method to get articles

--- app/models/collection_list.rb
+++ app/models/collection_list.rb
@@ -7,4 +7,8 @@ class CollectionList < ApplicationRecord
   has_many :articles, through: :collection_list_articles, dependent: :destroy
 
   acts_as_taggable_on :tags
+
+  def find_articles
+    Article.where("created_at >= ?", 1.week.ago.utc).order(:page_views_count).limit(5)
+  end
 end
