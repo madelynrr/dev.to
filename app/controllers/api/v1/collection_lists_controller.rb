084ea6f1d26a4da7f3d265e7d8d957d601c6dd06
Becky Robran <rerobran@gmail.com>
2020-03-24 20:24:18 -0600

    updates the create action to add an article to a collection_list and renders json

--- app/controllers/api/v1/collection_lists_controller.rb
+++ app/controllers/api/v1/collection_lists_controller.rb
@@ -1,31 +1,19 @@
 class Api::V1::CollectionListsController < ApplicationController
-
   def create
-    coll = current_user.collection_lists.create(collection_list_params)
-    # article = Article.first
-
-    # x = coll.collection_list_articles << article
-
-
-
-
+    collection = current_user.collection_lists.create(collection_list_params)
+    if collection.save
+      articles = collection.find_articles
+      collection.articles << articles
+      render json: { collections: current_user.collection_lists }
+      # render json: { name: collection.name, tag_list: collection.tag_list }
+    else
+      response.status = 401
+    end
   end
 
   private
+
   def collection_list_params
     params.permit(:name, tag_list: [])
   end
-
-
-  # def create
-  #   collection = current_user.collection_list.create(collection_list_params)
-  #   if collection.save
-  #     response.status = 200
-  #     render json: CollectionListSerializer.new(collection)
-  #   else
-  #     response.status = 401
-  #     render json: {description: user.errors.full_messages.to_sentence}
-  #   end
-  # end
-
 end
