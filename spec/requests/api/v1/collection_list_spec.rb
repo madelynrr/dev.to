6cf8eb43ec2baee31530520c7f045af3577481aa
Becky Robran <rerobran@gmail.com>
2020-03-24 20:26:50 -0600

    updates testing

--- spec/requests/api/v1/collection_list_spec.rb
+++ spec/requests/api/v1/collection_list_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 RSpec.describe API do
   describe "Collection Lists" do
     let(:user) { create(:user) }
-    let(:article) { create(:article) }
 
     before do
+      create(:article)
       sign_in user
     end
 
@@ -19,6 +19,11 @@ RSpec.describe API do
 
       expect(response).to be_successful
       expect(CollectionList.count).to eq(1)
+      data = JSON.parse(response.body)
+      expect(data["collections"].first["name"]).to eq("Javascript")
+      expect(data["collections"].first["tag_list"]).to eq(["Javascript"])
+      # expect(data["name"]).to eq("Javascript")
+      # expect(data["tag_list"]).to eq(["Javascript"])
     end
   end
 end
