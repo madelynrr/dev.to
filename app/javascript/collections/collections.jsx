7cc8c4e6698f76d817110238bd0d521fb8ca080b
Carla Geglio <carla.geglio@gmail.com>
2020-03-27 08:44:34 -0600

    Add collectuons component for show page

---
+++ app/javascript/collections/collections.jsx
@@ -0,0 +1,12 @@
+import { h, Component } from 'preact';
+// import { PropTypes } from 'preact-compat';
+// import debounce from 'lodash.debounce';
+
+export class Collections extends Component {
+
+  render() {
+    return (
+      <div>hi</div>
+    )
+  }
+}
