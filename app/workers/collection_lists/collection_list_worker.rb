abebedc4cf44317583d543716eeca78c399783a6
Madelyn Romero <m.romero013@gmail.com>
2020-03-27 14:14:41 -0600

    Find articles for each new collection

--- app/workers/collection_lists/collection_list_worker.rb
+++ app/workers/collection_lists/collection_list_worker.rb
@@ -9,7 +9,7 @@ module CollectionLists
         if (collection.created_at + 2.seconds) < Time.now
           new_collection = CollectionList.create(name: collection.name, user_id: collection.user_id, tag_list: collection.tag_list)
 
-          # new_collection.find_articles
+          new_collection.find_articles
         end
       end
     end
