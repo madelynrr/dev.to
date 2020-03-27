ebf806b2880802af9ff994362c4c1991a5219770
Madelyn Romero <m.romero013@gmail.com>
2020-03-27 13:17:27 -0600

    Refactor so collection list worker recognized

--- spec/workers/collection_lists/collection_list_worker_spec.rb
+++ spec/workers/collection_lists/collection_list_worker_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe CollectionLists::CollectionListWorker, type: :job do
+  subject(:worker) { described_class.new }
+
   describe "#perform" do
     it "creates a new collection list if the collection is older than three seconds" do
       create_list(:collection_list, 2)
@@ -8,8 +10,6 @@ RSpec.describe CollectionLists::CollectionListWorker, type: :job do
       create(:collection_list)
 
       expect(CollectionList.all).to eq(3)
-
-      worker = CollectionListWorker.new
 
       worker.perform
 
