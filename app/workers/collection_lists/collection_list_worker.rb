e524aec64057015e3e52755a2357d40ad3444066
Madelyn Romero <m.romero013@gmail.com>
2020-03-27 14:11:30 -0600

    Create initial method to iterate through all collection lists

--- app/workers/collection_lists/collection_list_worker.rb
+++ app/workers/collection_lists/collection_list_worker.rb
@@ -1,5 +1,18 @@
 module CollectionLists
+
   class CollectionListWorker
     include Sidekiq::Worker
+
+    def perform
+      collections = CollectionList.all
+      collections.each do |collection|
+        # if (collection.created_at + 2.seconds) < Time.now
+          # new_collection = CollectionList.create(name: collection.name, user_id: collection.user_id, tag_list: collection.tag_list)
+
+          # new_collection.find_articles
+        end
+      end
+    end
+
   end
 end
