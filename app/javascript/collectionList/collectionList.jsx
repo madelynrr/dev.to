cb91491389a448f8b426eddab0eb2666f3e4e0fd
Carla Geglio <carla.geglio@gmail.com>
2020-03-28 10:52:17 -0600

    Format published date on collection articles

--- app/javascript/collectionList/collectionList.jsx
+++ app/javascript/collectionList/collectionList.jsx
@@ -1,36 +1,34 @@
 import { h } from 'preact';
-import { PropTypes } from 'preact-compat';
 import { ItemListItem } from '../src/components/ItemList/ItemListItem';
+import { PropTypes } from 'preact-compat';
 
 export const CollectionList = ({ articles, name }) => {
-  const parsedData = JSON.parse(articles);
+
+  let parsedData = JSON.parse(articles)
 
   const itemsToRender = parsedData.map(article => {
-    return <ItemListItem item={article} />;
-  });
+    const formattedDate = article.published_at.split('').splice(0, 10);
+    const months = ['January ', 'February ', 'March ', 'April ', 'May ', 'June ', 'July ', 'August ', 'September ', 'October ', 'November ', 'December ']
+    let y = formattedDate.splice(0, 4).join('');
+    let m = formattedDate.splice(1, 2).join('');
+    let d = formattedDate.splice(2, 3).join('') + ', ';
+    m = months[Number(m) - 1]
+    article.publishedDate = (m + d + y);
+    return <ItemListItem item={article}></ItemListItem>
+  })
 
   return (
-    <div className="item-list-container collection-list-page">
-      <div className="items-container collection-list-page">
-        <div className="results results--loaded">
-          <div className="results-header">
-            {name}
-            {' '}
-            Articles
-          </div>
-          {!parsedData.length ? (
-            <p>No articles match the selected tags :(</p>
-          ) : (
-            itemsToRender
-          )}
+    <div className="item-list-container collection-list-page" >
+      <div className="items-container collection-list-page" >
+      <div className="results results--loaded">
+        <div className="results-header">{name} Articles</div>
+        {!parsedData.length ? <p>No articles match the selected tags :(</p> : itemsToRender}
         </div>
-        <a className="collection-back-btn" href="/readinglist">
-          Back
-        </a>
+        <a className="collection-back-btn" href={"/readinglist"}>Back</a>
       </div>
     </div>
-  );
-};
+  )
+}
 
 CollectionList.propTypes = {
   articles: PropTypes.arrayOf(PropTypes.object).isRequired,
