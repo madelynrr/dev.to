55cb6c4f02f77bdc6ddeb044a35a7d52f2e69041
Carla Geglio <carla.geglio@gmail.com>
2020-03-28 09:59:56 -0600

    Add collection list name to controller

--- app/controllers/collection_lists_controller.rb
+++ app/controllers/collection_lists_controller.rb
@@ -1,6 +1,8 @@
 class CollectionListsController < ApplicationController
   def show
     collection = CollectionList.find(params[:id])
-    @articles = collection.articles.includes([:taggings]).to_json
+    @articles =
+      collection.articles.includes([:taggings]).to_json
+    @name = collection.name
   end
 end
