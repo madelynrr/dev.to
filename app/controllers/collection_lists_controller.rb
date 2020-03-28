d7bb86a7a0e19a2df808cbf61d3061ca74dc5e81
Becky Robran <rerobran@gmail.com>
2020-03-28 12:04:55 -0600

    adds .includes for articles and updates the application controller

--- app/controllers/collection_lists_controller.rb
+++ app/controllers/collection_lists_controller.rb
@@ -1,6 +1,7 @@
-class CollectionListsController < ApplicationRecord
+class CollectionListsController < ApplicationController
   def show
     collection = CollectionList.find(params[:id])
-    @articles = collection.articles.to_json
+    @articles = collection.articles.includes([:taggings]).to_json
+    @name = collection.name
   end
 end
