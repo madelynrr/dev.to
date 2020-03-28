6b26b0c61bb52ef2b129717c339b3280031d43af
Becky Robran <rerobran@gmail.com>
2020-03-28 12:11:44 -0600

    adds test for find articles method

--- spec/models/collection_list_spec.rb
+++ spec/models/collection_list_spec.rb
@@ -10,4 +10,25 @@ RSpec.describe CollectionList do
     it { is_expected.to have_many :collection_list_articles }
     it { is_expected.to have_many(:articles).through :collection_list_articles }
   end
+
+  describe "Methods" do
+
+      before do
+        @a1 = create(:article, page_views_count: 2, tags: ['java', 'react', 'sql']) #should be in list
+        @a6 = create(:article, page_views_count: 1, tags: ['java', 'react', 'sql']) #should be in list
+        @a2 = create(:article, tags: ['ruby', 'react', 'postgress']) #should not be list
+        @a3 = create(:article, page_views_count: 4, tags: ['javascript', 'rails', 'sql']) #should be in list
+        @a4 = create(:article, tags: ['graphql', 'react', 'sql']) #should not be list
+        @a5 = create(:article, created_at: 'Fri, 27 Jan 2020 19:21:25 SAST +02:00,', tags: ['java', 'react', 'sql']) #should not be list
+        @user = create(:user)
+        @collection = @user.collection_lists.create(name: "Hello Java", tag_list: ['java', 'rails'])
+      end
+
+      it 'should be able to find articles for a collection' do
+        expect(@collection.find_articles.length).to eq(3)
+        expect(@collection.find_articles[0].id).to eq(@a3.id)
+        expect(@collection.find_articles[1].id).to eq(@a1.id)
+        expect(@collection.find_articles[2].id).to eq(@a6.id)
+      end
+  end
 end
