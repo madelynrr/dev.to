d4a44afc990d06d275216099c9249eb236f77c19
Madelyn Romero <m.romero013@gmail.com>
2020-03-28 14:09:41 -0600

    Shovel new articles into collection

--- app/workers/collection_lists/collection_list_worker.rb
+++ app/workers/collection_lists/collection_list_worker.rb
@@ -9,10 +9,20 @@ module CollectionLists
         if (collection.created_at + 2.seconds) < Time.now
           new_collection = CollectionList.create(name: collection.name, user_id: collection.user_id, tag_list: collection.tag_list)
 
-          new_collection.find_articles
+          articles = new_collection.find_articles
+          new_collection.articles << articles
         end
       end
     end
-
   end
+
+  CollectionListWorker.perform_at(1.minutes.from_now)
+
+#   Sidekiq::Cron::Job.create(name: 'CollectionList Update - every day', cron: '0 0 * * *', class: CollectionListWorker)
+#
+#   if job.valid?
+#     job.save
+#   else
+#     puts job.errors
+#   end
 end
