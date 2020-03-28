8b89509ba877c218334d4572083f0424d632d99b
Carla Geglio <carla.geglio@gmail.com>
2020-03-28 09:12:39 -0600

    Rename collection component to collection list

---
+++ app/javascript/collectionList/collectionList.jsx
@@ -0,0 +1,33 @@
+import { h } from 'preact';
+import { PropTypes } from 'preact-compat';
+import { ItemListItem } from '../src/components/ItemList/ItemListItem';
+
+export const CollectionList = ({ articles }) => {
+  const parsedData = JSON.parse(articles);
+
+  const itemsToRender = parsedData.map(article => {
+    return <ItemListItem item={article} />;
+  });
+
+  return (
+    <div className="item-list-container collection-list-page">
+      <div className="items-container collection-list-page">
+        <div className="results results--loaded">
+          <div className="results-header">Collection Articles</div>
+          {!parsedData.length ? (
+            <p>No articles match the selected tags :(</p>
+          ) : (
+            itemsToRender
+          )}
+        </div>
+        <a className="collection-back-btn" href="/readinglist">
+          Back
+        </a>
+      </div>
+    </div>
+  );
+};
+
+CollectionList.propTypes = {
+  articles: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
